70a1836511e726902d2a2ecccd36c9bab5284295
grace-mugoiri <gracemugoiri@gmail.com>
2020-08-04 12:55:14 +0300

    add user controller and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+		if @users
+			render json: { users: @users }
+		else
+			render json: { status: 500, errors: ['no users found']}
+		end
+  end
+
+	def show
+		@user = User.find(params[:id])
+		if @user
+			render json: {user: @user }
+		else
+			render json: {status: 500, errors: ['user not found']}
+		end
+  end
+
+	def create
+		@user = User.new(user_params)
+		# puts @user
+		if @user.save
+		# 	# login!
+			render json: @user, status: 200
+		else
+			render json: { error: 'Unable to create user' }, status: 400
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user
+			@user.destroy
+			render json: {message: 'User deleted'}, status: 200
+		else
+			render json: {error: 'Unable to delete user!'}, status: 400
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	resources :users, only: [:create, :show, :index, :destroy]
+
+
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	get '/logged_in', to: 'sessions#is_logged_in'
+
 end
