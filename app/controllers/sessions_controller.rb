63bc5956c996c09432341df14f4e3ad51bfdaea5
grace-mugoiri <gracemugoiri@gmail.com>
2020-08-04 13:29:40 +0300

    verify session routes are working and fix the logged_in? error missing question mark on line 15

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
 
 	def create
-		user = User.find_by(username: params[:username]["username"])
-		if user && user.authenticate(session_params[:password])
+		@user = User.find_by(email: session_params[:email])
+		if @user && @user.authenticate(session_params[:password])
 		# if user session[:user_id] = user.id]
 			login!
 			render json: { logged_in: true, user: @user }
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 	end
 
 	def is_logged_in?
-		if logged_in && current_user
+		if logged_in? && current_user
 			render json: { logged_in: true, user: current_user }
 		else
 			render json: { logged_in: false, message: 'no such user'}
@@ -27,6 +27,6 @@ class SessionsController < ApplicationController
 	private
 
 	def session_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
 	post '/login', to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
-	get '/logged_in', to: 'sessions#is_logged_in'
+	get '/logged_in', to: 'sessions#is_logged_in?'
 
 end
